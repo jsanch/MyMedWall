9b4bca91ba0e13b608c0338c837caa31ee436cea
Jaime Sanchez <itunes@wr-130-64-96-103.medford.tufts.edu>
2013-09-25 10:23:19 -0400

    second commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130925035109) do
+ActiveRecord::Schema.define(:version => 20130925132432) do
 
   create_table "locations", :force => true do |t|
     t.string   "city"
@@ -41,9 +41,11 @@ ActiveRecord::Schema.define(:version => 20130925035109) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
+  add_index "users", ["username"], :name => "index_users_on_username", :unique => true
 
 end
